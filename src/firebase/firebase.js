57fbfc3770556645d280c9dd37dc69f12ee17f65
Shahzod95 <aktamovsh6975@gmail.com>
2022-08-12 19:55:12 +0500

    Start is tik-tok clone app

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAQo_BmzCUDAcGFqVcenLJWO8rU0gNbnVk",
+    authDomain: "tik-tok-clone-4c7d9.firebaseapp.com",
+    projectId: "tik-tok-clone-4c7d9",
+    storageBucket: "tik-tok-clone-4c7d9.appspot.com",
+    messagingSenderId: "392209062889",
+    appId: "1:392209062889:web:a64d418e95a1ae7f570978",
+    measurementId: "G-P6YRP8TW51"
+  };
